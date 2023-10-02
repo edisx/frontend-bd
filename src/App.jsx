4a35477f373fc1aba4364d61fa2c6421ffb63f8c
brawlstars15 <eduardas256@gmail.com>
2023-10-02 06:38:20 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+export default App;
