5df6c20abb354ca7bb1374ce68ebdfa950dff9a3
brawlstars15 <eduardas256@gmail.com>
2024-01-14 00:58:06 +0200

    Add project information

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -27,7 +27,7 @@ const HomeScreen = () => {
   } = productAll;
 
   useEffect(() => {
-    dispatch(fetchAllProducts({ keyword, page , category_id}));
+    dispatch(fetchAllProducts({ keyword, page, category_id }));
   }, [dispatch, keyword, page, category_id]);
 
   const handlePageChange = (event, value) => {
@@ -36,27 +36,58 @@ const HomeScreen = () => {
 
   if (loading === "loading") return <CircularProgress />;
   if (error) return <Alert severity="error">{error}</Alert>;
-  if (products.length === 0) return <Alert severity="info">No products with the keyword "{keyword}"</Alert>;
+  if (products.length === 0)
+    return (
+      <Alert severity="info">No products with the keyword "{keyword}"</Alert>
+    );
 
   return (
-    <div className="container mx-auto px-4 mt-9 flex flex-col min-h-screen flex-grow justify-between">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {products &&
-          products.length > 0 &&
-          products.map((product) => (
-            <div key={product.id}>
-              <Product product={product} />
-            </div>
-          ))}
+    <>
+      <Alert severity="info">
+        <div>
+          <p>
+            This project is a demonstration prototype developed to showcase a
+            functional e-commerce platform with 3D product configuration
+          </p>
+          <div className="flex flex-row space-x-4 mt-2">
+            <a
+              href="https://github.com/edisx/frontend-bd"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
+            >
+              Frontend Repository
+            </a>
+            <a
+              href="https://github.com/edisx/backend-bd"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
+            >
+              Backend Repository
+            </a>
+          </div>
+        </div>
+      </Alert>
+      <div className="container mx-auto px-4 mt-9 flex flex-col min-h-screen flex-grow justify-between">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {products &&
+            products.length > 0 &&
+            products.map((product) => (
+              <div key={product.id}>
+                <Product product={product} />
+              </div>
+            ))}
+        </div>
+        <div className="flex justify-center mb-8">
+          <Pagination
+            count={productPages}
+            page={productPage}
+            onChange={handlePageChange}
+          />
+        </div>
       </div>
-      <div className="flex justify-center mb-8">
-        <Pagination
-          count={productPages}
-          page={productPage}
-          onChange={handlePageChange}
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
